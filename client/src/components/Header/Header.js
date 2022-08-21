42f85c2f92fac0289d0b2fd7aa7c7a8e50eeb7cc
rashimotwani <motwani.raashi@gmail.com>
2022-08-21 20:08:36 +0530

    frontent preLogin

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,35 +1,45 @@
 import React, { Component, Fragment } from "react";
-import classes from "./Header.module.css"
-import { Button, Navbar, Container } from 'react-bootstrap';
+import classes from "./Header.module.css";
+import { Button, Navbar, Container } from "react-bootstrap";
 import { ReactDOM } from "react";
 import { withRouter } from "react-router";
+import Logo from "../../../src/logo.png";
 
 class Header extends Component {
-    state = {};
-    render() {
-        return <Fragment>
-            <Navbar expand="lg" bg="#e6f8ff" variant="light" className={classes.Navbar}>
-                <Container>
-                    <Navbar.Brand href="/">
-                        <div className={classes.LeftContainer}>
-                            <img
-                                alt=""
-                                src="../../logo.svg"
-                                width="30"
-                                height="30"
-                                className="align-top"
-                            />{' '}
-                            <div>
-                                <span className={classes.InstiName}>Indian Institute of Technology</span>
-                                <br />
-                                <span className={classes.HeadingName}>Admissions</span>
-                            </div>
-                        </div>
-                    </Navbar.Brand>
-                </Container>
-            </Navbar>
-        </Fragment>;
-    }
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <Navbar
+          expand="lg"
+          bg="#e6f8ff"
+          variant="light"
+          className={classes.Navbar}
+        >
+          <Container>
+            <Navbar.Brand href="/">
+              <div className={classes.LeftContainer}>
+                <img
+                  alt=""
+                  src={Logo}
+                  width="100"
+                  height="100"
+                  className="align-top"
+                />{" "}
+                <div>
+                  <span className={classes.InstiName}>
+                    Indian Institute of Technology
+                  </span>
+                  <br />
+                  <span className={classes.HeadingName}>Admissions</span>
+                </div>
+              </div>
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+      </Fragment>
+    );
+  }
 }
 
 export default withRouter(Header);
